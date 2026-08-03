7e4249c9e0da59e711be450286edfbbb66022484
Rodimus Prime <RodimusFVC@gmail.com>
2026-08-03 13:49:12 -0500

    Vanguard Fixes

--- verilator/obj_dir/hd38880_tb.d
+++
@@ -1,5 +0,0 @@
-hd38880_tb.o: ../hd38880_tb.cpp Vhd38880_top_sim.h \
- /usr/local/share/verilator/include/verilated_heavy.h \
- /usr/local/share/verilator/include/verilated.h \
- /usr/local/share/verilator/include/verilatedos.h \
- /usr/local/share/verilator/include/verilated.h
